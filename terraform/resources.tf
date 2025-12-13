9a08376c04369d8151853cda905a116dcd96e346
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-14 00:51:41 +0530

    terraform test 2

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -17,7 +17,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
 }
 
 resource "aws_iam_role_policy_attachment" "ssm" {
-  role      = aws_iam_role.ec2_ssm_role.name
+  role      = data.aws_iam_role.ec2_ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
