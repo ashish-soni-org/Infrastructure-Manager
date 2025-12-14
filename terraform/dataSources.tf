6bd66572527b39e9a83d290d2060ab0a98a3f6eb
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-15 03:34:22 +0530

    docker test 1

--- terraform/dataSources.tf
+++ terraform/dataSources.tf
@@ -12,9 +12,11 @@ data "aws_security_group" "Production_Security_Group" {
     ENV = "PROD"
   }
 }
+
 data "aws_iam_role" "ec2_ssm_role" {
   name = "Production-Server-SSM-Role"
 }
+
 data "aws_iam_instance_profile" "ec2_profile" {
   name = "Production-Server-SSM-Profile"
 }
