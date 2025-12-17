f76f90118add0b003502ada6266f30a73ef54223
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-17 18:54:17 +0530

    Ansible-vault test 37

--- ansible/main.sh
+++ ansible/main.sh
@@ -4,7 +4,7 @@
 DOMAIN=$1
 CONFIG_FILE=$2
 
-# ./basic_config.sh "$DOMAIN" "$CONFIG_FILE"
+./basic_config.sh "$DOMAIN" "$CONFIG_FILE"
 
 # # project config variables
 COMMENTS_ARRAY=$3
@@ -14,26 +14,18 @@ IP=$6
 PORTS_ARRAY=$7
 PROXY_HEADERS=$8
 
-echo CONFIG_FILE: $CONFIG_FILE > logs1
-echo COMMENTS_ARRAY: $COMMENTS_ARRAY >> logs2
-echo LOCATIONS_ARRAY: $LOCATIONS_ARRAY >> logs2
-echo REWRITES_ARRAY: $REWRITES_ARRAY >> logs2
-echo IP: $IP >> logs2
-echo PORTS_ARRAY: $PORTS_ARRAY >> logs2
-echo PROXY_HEADERS: $PROXY_HEADERS >> logs2
 
+SIZE=${#COMMENTS_ARRAY[@]}
 
-# SIZE=${#COMMENTS_ARRAY[@]}
+for i in "${!COMMENTS_ARRAY[@]}"; do
 
-# for i in "${!COMMENTS_ARRAY[@]}"; do
+    COMMENT="${COMMENTS_ARRAY[$i]}" 
+    LOCATION="${LOCATIONS_ARRAY[$i]}" 
+    REWRITE="${REWRITES_ARRAY[$i]}"
+    PORT="${PORTS_ARRAY[$i]}" 
 
-#     COMMENT="${COMMENTS_ARRAY[$i]}" 
-#     LOCATION="${LOCATIONS_ARRAY[$i]}" 
-#     REWRITE="${REWRITES_ARRAY[$i]}"
-#     PORT="${PORTS_ARRAY[$i]}" 
+    # proxy pass
+    PROXY_PASS="${IP%/}${PORT}/"
 
-#     # proxy pass
-#     PROXY_PASS="${IP%/}${PORT}/"
-
-#     ./project_config.sh "$CONFIG_FILE" "$COMMENT" "$LOCATION" "$REWRITE" "$PROXY_PASS" "${PROXY_HEADERS[@]}"    
-# done
+    ./project_config.sh "$CONFIG_FILE" "$COMMENT" "$LOCATION" "$REWRITE" "$PROXY_PASS" "${PROXY_HEADERS[@]}"    
+done
--- ansible/project_config.sh
+++ ansible/project_config.sh
@@ -7,47 +7,40 @@ REWRITE=$4
 PROXY_PASS=$5
 PROXY_HEADERS=("${@:6}")
 
-echo CONFIG_FILE : $CONFIG_FILE > logs2
-echo COMMENT : $COMMENT > logs2
-echo LOCATION : $LOCATION >> logs2
-echo REWRITE : $REWRITE >> logs2
-echo PROXY_PASS : $PROXY_PASS >> logs2
-echo PROXY_HEADERS : $PROXY_HEADERS >> logs2
+# create proxy header lines
+PROXY_SET_HEADERS=""
 
-# # create proxy header lines
-# PROXY_SET_HEADERS=""
+SIZE=${#PROXY_HEADERS[@]}
 
-# SIZE=${#PROXY_HEADERS[@]}
+for i in "${!PROXY_HEADERS[@]}"; do
 
-# for i in "${!PROXY_HEADERS[@]}"; do
+    header="${PROXY_HEADERS[$i]}"
+    PROXY_SET_HEADERS+=$'\t\t'
+    PROXY_SET_HEADERS+="proxy_set_header $header;"
 
-#     header="${PROXY_HEADERS[$i]}"
-#     PROXY_SET_HEADERS+=$'\t\t'
-#     PROXY_SET_HEADERS+="proxy_set_header $header;"
+    # if it's not last value in array, add required spaces
+    if [[ $i -lt $((SIZE - 1)) ]]; then
+        PROXY_SET_HEADERS+=$'\n'
+    fi
+done
 
-#     # if it's not last value in array, add required spaces
-#     if [[ $i -lt $((SIZE - 1)) ]]; then
-#         PROXY_SET_HEADERS+=$'\n'
-#     fi
-# done
+# create new mapping block
+NEW_MAPPING_BLOCK="
+    $COMMENT
+    location $LOCATION {"
 
-# # create new mapping block
-# NEW_MAPPING_BLOCK="
-#     $COMMENT
-#     location $LOCATION {"
+if [[ -n "$REWRITE" ]]; then
+    NEW_MAPPING_BLOCK+="
+        rewrite $REWRITE;"
+fi
 
-# if [[ -n "$REWRITE" ]]; then
-#     NEW_MAPPING_BLOCK+="
-#         rewrite $REWRITE;"
-# fi
+NEW_MAPPING_BLOCK+="
+        proxy_pass $PROXY_PASS;
+$PROXY_SET_HEADERS
+    }"
 
-# NEW_MAPPING_BLOCK+="
-#         proxy_pass $PROXY_PASS;
-# $PROXY_SET_HEADERS
-#     }"
-
-# # add new mapping block to last second line
-# head -n -1 "$CONFIG_FILE" > "$CONFIG_FILE.tmp"
-# echo "$NEW_MAPPING_BLOCK" >> "$CONFIG_FILE.tmp"
-# echo "}" >> "$CONFIG_FILE.tmp"
-# mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
+# add new mapping block to last second line
+head -n -1 "$CONFIG_FILE" > "$CONFIG_FILE.tmp"
+echo "$NEW_MAPPING_BLOCK" >> "$CONFIG_FILE.tmp"
+echo "}" >> "$CONFIG_FILE.tmp"
+mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
