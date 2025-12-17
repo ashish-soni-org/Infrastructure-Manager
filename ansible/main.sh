c6c6ce175db1100e30630ed403ae70dce19414ee
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-17 18:18:33 +0530

    Ansible-vault test 28

--- ansible/main.sh
+++ ansible/main.sh
@@ -6,26 +6,26 @@ CONFIG_FILE=$2
 
 ./basic_config.sh "$DOMAIN" "$CONFIG_FILE"
 
-# project config variables
-COMMENTS_ARRAY=$3
-LOCATIONS_ARRAY=$4
-REWRITES_ARRAY=$5
-IP=$6
-PORTS_ARRAY=$7
-PROXY_HEADERS=$8
+# # project config variables
+# COMMENTS_ARRAY=$3
+# LOCATIONS_ARRAY=$4
+# REWRITES_ARRAY=$5
+# IP=$6
+# PORTS_ARRAY=$7
+# PROXY_HEADERS=$8
 
 
-SIZE=${#COMMENTS_ARRAY[@]}
+# SIZE=${#COMMENTS_ARRAY[@]}
 
-for i in "${!COMMENTS_ARRAY[@]}"; do
+# for i in "${!COMMENTS_ARRAY[@]}"; do
 
-    COMMENT="${COMMENTS_ARRAY[$i]}" 
-    LOCATION="${LOCATIONS_ARRAY[$i]}" 
-    REWRITE="${REWRITES_ARRAY[$i]}"
-    PORT="${PORTS_ARRAY[$i]}" 
+#     COMMENT="${COMMENTS_ARRAY[$i]}" 
+#     LOCATION="${LOCATIONS_ARRAY[$i]}" 
+#     REWRITE="${REWRITES_ARRAY[$i]}"
+#     PORT="${PORTS_ARRAY[$i]}" 
 
-    # proxy pass
-    PROXY_PASS="${IP%/}${PORT}/"
+#     # proxy pass
+#     PROXY_PASS="${IP%/}${PORT}/"
 
-    ./project_config.sh "$CONFIG_FILE" "$COMMENT" "$LOCATION" "$REWRITE" "$PROXY_PASS" "${PROXY_HEADERS[@]}"    
-done
+#     ./project_config.sh "$CONFIG_FILE" "$COMMENT" "$LOCATION" "$REWRITE" "$PROXY_PASS" "${PROXY_HEADERS[@]}"    
+# done
