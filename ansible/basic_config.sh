8d45795d93899197e56b48b38004b5f5e6e5f5c4
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-17 18:26:49 +0530

    Ansible-vault test 29

--- ansible/basic_config.sh
+++ ansible/basic_config.sh
@@ -3,11 +3,8 @@
 DOMAIN=$1
 CONFIG_FILE=$2
 
-echo DOMAIN: $DOMAIN > logs1
-echo CONFIG_FILE: $CONFIG_FILE >> logs1
-
-# cat <<EOF > $CONFIG_FILE
-# server {
-#     server_name $DOMAIN;
-# }
-# EOF
+cat <<EOF > $CONFIG_FILE
+server {
+    server_name $DOMAIN;
+}
+EOF
