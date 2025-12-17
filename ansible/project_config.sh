1cf37d77bc2cc8b39e6d336310ee59df2173ee1f
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-17 18:10:56 +0530

    Ansible-vault test 26

--- ansible/project_config.sh
+++ ansible/project_config.sh
@@ -14,40 +14,40 @@ echo REWRITE : $REWRITE >> logs2
 echo PROXY_PASS : $PROXY_PASS >> logs2
 echo PROXY_HEADERS : $PROXY_HEADERS >> logs2
 
-# create proxy header lines
-PROXY_SET_HEADERS=""
+# # create proxy header lines
+# PROXY_SET_HEADERS=""
 
-SIZE=${#PROXY_HEADERS[@]}
+# SIZE=${#PROXY_HEADERS[@]}
 
-for i in "${!PROXY_HEADERS[@]}"; do
+# for i in "${!PROXY_HEADERS[@]}"; do
 
-    header="${PROXY_HEADERS[$i]}"
-    PROXY_SET_HEADERS+=$'\t\t'
-    PROXY_SET_HEADERS+="proxy_set_header $header;"
+#     header="${PROXY_HEADERS[$i]}"
+#     PROXY_SET_HEADERS+=$'\t\t'
+#     PROXY_SET_HEADERS+="proxy_set_header $header;"
 
-    # if it's not last value in array, add required spaces
-    if [[ $i -lt $((SIZE - 1)) ]]; then
-        PROXY_SET_HEADERS+=$'\n'
-    fi
-done
+#     # if it's not last value in array, add required spaces
+#     if [[ $i -lt $((SIZE - 1)) ]]; then
+#         PROXY_SET_HEADERS+=$'\n'
+#     fi
+# done
 
-# create new mapping block
-NEW_MAPPING_BLOCK="
-    $COMMENT
-    location $LOCATION {"
+# # create new mapping block
+# NEW_MAPPING_BLOCK="
+#     $COMMENT
+#     location $LOCATION {"
 
-if [[ -n "$REWRITE" ]]; then
-    NEW_MAPPING_BLOCK+="
-        rewrite $REWRITE;"
-fi
+# if [[ -n "$REWRITE" ]]; then
+#     NEW_MAPPING_BLOCK+="
+#         rewrite $REWRITE;"
+# fi
 
-NEW_MAPPING_BLOCK+="
-        proxy_pass $PROXY_PASS;
-$PROXY_SET_HEADERS
-    }"
+# NEW_MAPPING_BLOCK+="
+#         proxy_pass $PROXY_PASS;
+# $PROXY_SET_HEADERS
+#     }"
 
-# add new mapping block to last second line
-head -n -1 "$CONFIG_FILE" > "$CONFIG_FILE.tmp"
-echo "$NEW_MAPPING_BLOCK" >> "$CONFIG_FILE.tmp"
-echo "}" >> "$CONFIG_FILE.tmp"
-mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
+# # add new mapping block to last second line
+# head -n -1 "$CONFIG_FILE" > "$CONFIG_FILE.tmp"
+# echo "$NEW_MAPPING_BLOCK" >> "$CONFIG_FILE.tmp"
+# echo "}" >> "$CONFIG_FILE.tmp"
+# mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
