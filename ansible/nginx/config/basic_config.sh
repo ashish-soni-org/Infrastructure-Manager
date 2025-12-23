0b4c090a66323ba553f489c24e525a4f50485f38
Ashish Soni <ashishsoni295work@gmail.com>
2025-12-23 19:48:21 +0530

    UI Testing

--- ansible/nginx/config/basic_config.sh
+++ ansible/nginx/config/basic_config.sh
@@ -4,13 +4,12 @@ DOMAIN=$1
 CONFIG_FOLDER=$2
 CONFIG_FILE="$CONFIG_FOLDER/$DOMAIN"
 
-cat <<EOF > $CONFIG_FILE
+cat <<EOF > "$CONFIG_FILE"
 server {
     server_name $DOMAIN www.$DOMAIN;
 }
-EOF 
+EOF
 
 
-
-sudo chown root "$DOMAIN" 
-sudo chmod 0544 "$DOMAIN"
+sudo chown root:root "$CONFIG_FILE"
+sudo chmod 644 "$CONFIG_FILE"
